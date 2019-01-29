ada79ceac08b3398511e3072be156f3108a6b0d7
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-01-29 18:09:33 +0545

    Setting Up the Reducer

--- src/store/reducer/places.js
+++ src/store/reducer/places.js
@@ -5,8 +5,44 @@ const initialState = {
     selectedPlace: null
 }
 
-const reducer = (state= initialState, action) => {
+const reducer = (state= initial State, action) => {
     switch (action.type) {
+        case ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(),
+                    name: action.placeName,
+                    //image: placeImage
+                    image: {
+                        uri: "https://images.unsplash.com/photo-1503803548695-c2a7b4a5b875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
+                    }
+                  })
+            };
+
+        case DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                  }),
+                  selectedPlace: null
+            }
+
+        case SELECT_PLACE:
+            return {
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                  })
+            }
+
+        case DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+
+            }
+
         default:
             return state;
     }
