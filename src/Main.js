c5fc386e91b190cbd7ed4b56f036284cfe1fe3d2
sabinbogati1 <sabin.bogati@studentpartner.com>
2019-01-30 14:12:06 +0545

    Added third Party Libraries..

--- src/Main.js
+++ src/Main.js
@@ -7,63 +7,23 @@ import PlaceDetail from "./components/PlaceDetail/PlaceDetail";
 import { addPlace, deletePlace, selectPlace, deselectPlace} from "./store/actions/index";
 
  class Main extends Component {
-    // state = {
-    //     places: [],
-    //     selectedPlace: null
-    //   };
 
    placeAddedHandler = placeName => {
-
+     console.log("Place Added...");
      this.props.onAddPlace(placeName);
-
-        // this.setState(prevState => {
-        //   return {
-        //     places: prevState.places.concat({
-        //       key: Math.random(),
-        //       name: placeName,
-        //       //image: placeImage
-        //       image: {
-        //           uri: "https://images.unsplash.com/photo-1503803548695-c2a7b4a5b875?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"
-        //       }
-        //     })
-        //   };
-        // });
       };
 
    placeSelectedHandler = key => {
      this.props.onSelectPlace(key);
-
-    //  this.props.placeSelectedHandler
-
-    //     this.setState(prevState => {
-    //       return {
-    //         selectedPlace: prevState.places.find(place => {
-    //           return place.key === key;
-    //         })
-    //       }
-    //     })
       };
 
 
    placeDeletedHandler = () => {
      this.props.onDeletePlace();
-        //    this.setState(prevState => {
-        //   return {
-        //     places: prevState.places.filter(place => {
-        //       return place.key !== prevState.selectedPlace.key;
-        //     }),
-        //     selectedPlace: null
-        //   };
-        // });
       }
 
    modalClosedHandler = () => {
-
      this.props.onDeselectPlace();
-
-        // this.setState({
-        //   selectedPlace: null
-        // });
       }
 
       render() {
--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { StyleSheet, Modal, View, Image, Text, Button } from "react-native";
+import { StyleSheet, Modal, View, Image, Text, Button, TouchableOpacity } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const placeDetail = props => {
     let modalContent = null;
@@ -18,8 +19,13 @@ const placeDetail = props => {
         <Modal onRequestClose={props.onModalClosed} visible={props.selectedPlace !== null} animationType="slide">
         <View style={styles.modalContainer}>
             {modalContent}
-            <View>
-                <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+                <View>
+                    <TouchableOpacity onPress={props.onItemDeleted}>
+                        <View style={styles.deleteButton}>
+                            <Icon size={30} name="ios-trash" color="red" />
+                        </View>
+                    </TouchableOpacity>
+                {/* <Button title="Delete" color="red" onPress={props.onItemDeleted} /> */}
                 <Button title="Close" onPress={props.onModalClosed} />
             </View>
         </View>
@@ -39,6 +45,9 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         textAlign: "center",
         fontSize: 28
+    },
+    deleteButton: {
+        alignItems: "center"
     }
 })
 
